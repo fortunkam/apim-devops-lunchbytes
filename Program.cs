0068023917e9f4de93538c470899e46bd576f470
Matthew Fortunka <matthew@fortunka.co.uk>
2022-01-31 22:53:47 +0000

    added SwaggerGen to Release Build

--- Program.cs
+++ Program.cs
@@ -18,11 +18,8 @@ builder.Services.AddSingleton<ITodoRepository>(s => new TableStorageTodoReposito
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
